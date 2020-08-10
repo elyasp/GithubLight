bb586dfbfe343b3dcd267f8e61ebcab18075ab01
Tetsujin <elyasp@outlook.com>
2020-08-11 00:42:17 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { HomeView } from "./views";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeView />
+    </div>
+  );
+}
+
+export default App;
